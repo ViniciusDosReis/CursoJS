dcc54d44e046c468850cd55e2b0edfe38dac36f7
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-05-12 14:41:26 -0300

    nova repositorio

---
+++ aula12ex.copia/script.js
@@ -0,0 +1,9 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear
+    var res = document.getElementById('res')
+    var fano = document.querySelector('fdsex')
+    if () {
+        
+    }
+}
