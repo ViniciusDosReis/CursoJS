bd5892752802841422730a10b7126f7cc59c1616
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-05-14 15:29:21 -0300

    exercicio de verificador de idades

--- aula12ex.copia/script.js
+++ aula12ex.copia/script.js
@@ -1,9 +1,12 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear
-    var res = document.getElementById('res')
-    var fano = document.querySelector('fdsex')
-    if () {
+function verificar(){
+    var year = new Date()
+    var ano = year.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Alguma coisa tá errada')
+    } else {
+        var idade = ano - Number(fano.value)
         
     }
 }
