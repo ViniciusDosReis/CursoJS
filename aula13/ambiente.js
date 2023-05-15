f39ba274a7188ca42fdfac4c7a168e325bdc0a46
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-05-15 16:33:52 -0300

    iniciando os estudos em estruturas de Repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+var c = 1
+/*
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
